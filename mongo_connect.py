121346cbb06e9cc47f76b271faccf42a2bda820c
saptarshi1980 <saptarshi.edp@gmail.com>
2022-07-18 05:37:39 +0530

    Commit on 18/07/2022

---
+++ mongo_connect.py
@@ -0,0 +1,27 @@
+import pymongo
+import urllib.parse
+# I need to isntall some component, pls refer to mongodb official documentation
+
+
+#As i am mentioned @, I am parsing the user id and password
+username = urllib.parse.quote_plus('aisaptarshi')
+password = urllib.parse.quote_plus("Peter@123")
+conn_str = "mongodb+srv://{}:{}@cluster0.aean8.mongodb.net/?retryWrites=true&w=majority".format(username,password)
+
+# set a 5-second connection timeout
+client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+try:
+    print(client.server_info())
+    print(client)
+    dict1 = {
+
+        "name" : "samriddhi sanyal",
+        "school":"Assembly of God Church, Asansol"
+
+    }
+    db1 = client['mongotest']
+    coll = db1['test']
+    coll.insert_one(dict1)
+
+except Exception:
+    print("Unable to connect to the server.")
